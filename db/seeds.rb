3a5e809a3a8f098caeb11ef02474691821fe00d3
Marta Villar <martameneresvillar@hotmail.com>
2018-08-01 16:02:22 +0100

    rails-yelp-mvp not finished yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Nozolitalia',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'Italian',
+    phone_number: 5
+  },
+  {
+    name:         'Mcdonalds',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'Fast food',
+    phone_number: 5
+  },
+  {
+    name:         'H3',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'Fast food',
+    phone_number: 5
+  },
+  {
+    name:         'Portugalia',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'Meat',
+    phone_number: 5
+  },
+  {
+    name:         'Pasta no Basta',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'Italian',
+    phone_number: 5
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
